15c9f06076065cefd3ae72ffac1b7e7cd3bc3e5f
Paphop Karnchanavitthayakul <paphop45@hotmail.com>
2019-10-09 11:33:16 +0700

    Code optimization

--- earnBurnXLSX.py
+++ earnBurnXLSX.py
@@ -14,23 +14,15 @@ workbook = xlsxwriter.Workbook('randomEarnBurn.xlsx')
 worksheet = workbook.add_worksheet()
 
 i = 1
-records = 5
+records = 1000
 
 userCode = "b521012345","b521012346","b521012347","b521012348","b521012349","b521012350","b521012351","b521012352","b521012353","b521012354"
-studentCode = random.choice(userCode)
 
 actionType = "EARN","BURN"
-action = random.choice(actionType)
-
-value = randint(1,10)
 
 # Some data we want to write to the worksheet.
 header = (
     ['STUDENT CODE', 'ACTION', 'POINT'],
-)
-
-data = (
-	[studentCode, action, value],
 )
 
 # Start from the first cell. Rows and columns are zero indexed.
@@ -47,7 +39,7 @@ for code, action, point in (header):
 for i in range(1,records+1):
 	studentCode = random.choice(userCode)
 	action = random.choice(actionType)
-	value = randint(1,10)
+	value = randint(1,9999)
 	worksheet.write(row, col,     studentCode)
 	worksheet.write(row, col + 1, action)
 	worksheet.write(row, col + 2, value)
