aecafab27841cc41208ea1ea1a53a3d7b527a56e
Paphop Karnchanavitthayakul <paphop45@hotmail.com>
2019-10-09 09:33:10 +0700

    //  Workin Earn Burn mock csv generator

--- allEarn.py
+++ allEarn.py
@@ -1,17 +1,27 @@
 # This file generate for earn and burn page as csv file for bulk upload
 # Edit records for amount of records
+# Edit studentCode for studentcode
+# Edit randint for range of point in line 25
 
 import random
 import string
 import csv
 
-records=5
-action = 'EARN'
+from random import seed
+from random import randint
 
-fieldnames=['ACTION']
-writer = csv.DictWriter(open("MockEarn.csv", "w"), fieldnames=fieldnames)
+records=5
+
+studentCode = 'b521012347'
+
+actionType = 'EARN','BURN'
+
+fieldnames=['STUDENT CODE','ACTION','POINT']
+writer = csv.DictWriter(open("randomEarnBurn.csv", "w"), fieldnames=fieldnames)
 
 writer.writerow(dict(zip(fieldnames, fieldnames)))
 for i in range(1,records+1):
-  writer.writerow(dict([
-    ('ACTION', action)]))
+	action = random.choice(actionType)
+	value = randint(1,10)
+  	writer.writerow(dict([
+    ('STUDENT CODE', studentCode),('ACTION', action),('POINT', value)]))
